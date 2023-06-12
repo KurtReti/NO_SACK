e97a21ae97fb40673426cdfcbd2fe5f1f00e6976
KurtReti <kurt.reti@gmail.com>
2023-06-12 14:53:15 +1000

    init commit

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+
+
+    return(
+        <div className="header-container">
+            <HeaderItem />
+            <HeaderItem />
+            <HeaderItem />
+        </div>
+    );
+}
+
+const HeaderItem = () => {
+
+    return(
+        <div>
+            hi
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/dashboard.js
@@ -0,0 +1,13 @@
+import Header from './components/header';
+import './dashboard.css';
+
+const Dashboard = () => {
+
+    return (
+        <div className='container'>
+          <Header />
+        </div>
+    );
+};
+
+export default Dashboard;
